94b131948c72965bb6773b9ba37baf1be451035d
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-08-26 20:06:39 -0300

    Import

--- Aula2/Aulinha2/src/App.java
+++ Aula2/Aulinha2/src/App.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello, World!");
     }
 }
