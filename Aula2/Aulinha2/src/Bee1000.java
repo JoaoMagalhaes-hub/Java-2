7b97fe22a12c3aaee6fb999fcc13e12b33576767
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-08-26 21:11:22 -0300

    Atividades Beecrowd

--- Aula2/Aulinha2/src/Bee1000.java
+++ Aula2/Aulinha2/src/Bee1000.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
-public class App {
+public class Bee1000 {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World");
     }
 }
